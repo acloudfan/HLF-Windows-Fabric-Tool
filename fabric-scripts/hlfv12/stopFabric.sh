2a72e34db47728b5cf8875b1520c9ee3940c761b
acloudfan <raj@acloudfan.com>
2018-10-01 07:33:11 -0400

    updated

--- fabric-scripts/hlfv12/stopFabric.sh
+++ fabric-scripts/hlfv12/stopFabric.sh
@@ -19,8 +19,6 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Shut down the Docker containers that might be currently running.
-#DOCKER_FILE="${DIR}"/composer/docker-compose.yml
-
 # Changed by Raj
 #DOCKER_FILE="${DIR}"/composer/docker-compose.yml
 DOCKER_FILE="$(cygpath -pw "$DIR/composer/docker-compose.yml")"
